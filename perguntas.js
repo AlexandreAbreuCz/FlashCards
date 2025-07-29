c994589500b87f2290628c0df9d98f2ff86e14d0
AlexandreAbreuCz <alexandre.czarnieski@escola.pr.gov.br>
2025-07-29 03:58:29 +0000

    s

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'Onde pode ser usado java?',
+    'desenvolvimento de software empresarial, aplicativos móveis Android, aplicações web, jogos e sistemas embarcados. Em tudo Práticamente'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'oque o print() faz?',
+    'Exibe no consele oque for nescessario'
+)
